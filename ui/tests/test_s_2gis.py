24f6aa702e00b9064d4fcead1713c703dad4c459
Curasao <rudarya4@gmail.com>
2025-11-09 19:26:54 +0300

    Fix allure step

--- ui/tests/test_s_2gis.py
+++ ui/tests/test_s_2gis.py
@@ -15,11 +15,15 @@ def test_form_submission(setup_browser_filter):
         filter_page.rate_button()
         filter_page.price_button()
 
+@allure.feature("Фильтрация 2GIS")
+@allure.story("Поиск недвижимости")
 @allure.title("Поиск недвижимости по городу")
 def test_realty_search(setup_browser_realty):
     realty_page = Realty_Page()
     realty_page.realty_search("Тучково")
 
+@allure.feature("Фильтрация 2GIS")
+@allure.story("Поиск недвижимости")
 @allure.title("Открыть недвижимость и выставить фильтр цены")
 def test_realty_filters_price_and_rooms(setup_browser_realty):
     realty_page = Realty_Page()
@@ -35,6 +39,8 @@ def test_valid_login(setup_browser_form):
     form_page.form_open()
     form_page.form_telephone('+7965555555')
 
+@allure.feature("Форма пользователя 2GIS")
+@allure.story("Проверка номера телефона")
 @allure.title("Вход с некорректным номером")
 def test_unvalid_login(setup_browser_form):
     form_page = FormPage()

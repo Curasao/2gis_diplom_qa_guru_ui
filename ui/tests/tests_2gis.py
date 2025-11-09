c0af4877ef8caa9041e21351c641345c11b3a3de
Curasao <rudarya4@gmail.com>
2025-11-09 03:29:43 +0300

    Update allure step

--- ui/tests/tests_2gis.py
+++ ui/tests/tests_2gis.py
@@ -4,41 +4,35 @@ from ui.pages.test_2gis import Realty_Page
 from ui.pages.test_2gis import FilterPage
 from ui.pages.test_2gis import FormPage
 
-
+@allure.step("Открыть страницу и выполнить поиск кофеен")
 def test_form_submission(setup_browser_filter):
-    filter_page = FilterPage()
-
-    with allure.step("Открыть страницу и выполнить поиск кофеен"):
+        filter_page = FilterPage()
         filter_page.open()
         filter_page.fill_search("Кофейни")
-
-    with allure.step("Применить фильтры: доставка, рейтинг, цена"):
         filter_page.dostavka_button()
         filter_page.rate_button()
         filter_page.price_button()
 
-
+@allure.step("Поиск недвижимости по городу")
 def test_realty_search(setup_browser_realty):
-   with allure.step("Поиск недвижимости по городу"):
     realty_page = Realty_Page()
     realty_page.realty_search("Тучково")
 
-
+@allure.step("Открыть недвижимость и выставить фильтр цены")
 def test_realty_filters_price_and_rooms(setup_browser_realty):
- with allure.step("Открыть недвижимость и выставить фильтр цены"):
     realty_page = Realty_Page()
     realty_page.realty_open()
-    realty_page.realty_price_button(4000000, 800000)  # Пример установки цены
+    realty_page.realty_price_button(4000000, 800000)
 
+@allure.step("Вход с корректным номером")
 def test_valid_login(setup_browser_form):
- with allure.step("Вход с корректным номером"):
+
     form_page = FormPage()
     form_page.form_open()
     form_page.form_telephone('+7965555555')
 
-
+@allure.step("Вход с некорректным номером")
 def test_unvalid_login(setup_browser_form):
- with allure.step("Вход с некорректным номером"):
     form_page = FormPage()
     form_page.form_open()
     form_page.form_telephone('+555555555555555555555555555')
